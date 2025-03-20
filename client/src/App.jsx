f8ea3a01a5fa96bb0ed226f6af127dd94a5a804f
Monika Kiskinova <monikakiskinova@gmail.com>
2025-03-20 21:36:27 +0200

    Add footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 
 import Header from './components/header/Header'
 import Home from './components/home/Home'
+import Footer from './components/footer/Footer'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <Route index element={<Home />} />
     </Routes>
 
+    <Footer />
 
     </>
   )
---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+
+export default function Footer() {
+
+    return (
+        <footer>
+            <p>The Wake Spot Map. All rights reserved.</p>
+        </footer>
+    );
+};
