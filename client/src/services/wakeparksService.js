48b2ec3046e11ccfa6262212b0d8b1853b501043
Monika Kiskinova <monikakiskinova@gmail.com>
2025-03-23 11:32:21 +0200

    Add request util for fetching

--- client/src/services/wakeparksService.js
+++ client/src/services/wakeparksService.js
@@ -1,27 +1,17 @@
+import request from "../utils/request";
+
 const baseUrl = 'http://localhost:3030/jsonstore/wakeparks';
 
 export const wakeparkService = {
     async getAll() {
-        const response = await fetch(baseUrl);
-        const result = await response.json();
+        const result = await request.get(baseUrl);
         const parks = Object.values(result);        
         return parks;
     },
     async getOne(parkId) {
-        const response = await fetch(`${baseUrl}/${parkId}`);
-        const park = await response.json();        
-        return park;
+        return request.get(`${baseUrl}/${parkId}`);
     },
-    async create(parkData) {
-        const response = await fetch(baseUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(parkData),
-        });
-
-        const result = await response.json();
-        return result;
-    }, 
+    create (parkData) {
+        return request.post(baseUrl, parkData);
+    },
 }
