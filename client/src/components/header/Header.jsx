38ab55039d7cfc84584552a7d914ded11c32aaea
Monika Kiskinova <monikakiskinova@gmail.com>
2025-04-02 17:06:55 +0300

    Show hamburger menu in header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,12 +1,17 @@
 import { Link } from 'react-router'
 
 import './Header.css'
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../contexts/UserContext';
 // import styles from './Header.module.css'
 
 export default function Header() {
     const { username } = useContext(UserContext);
+    const [isActive, setIsActive] = useState(false)
+
+    const toggleIsActive = () => {
+        setIsActive((isActive) => !isActive);
+    }
 
     return (
         <header>
@@ -16,27 +21,32 @@ export default function Header() {
             <nav>
                 <div className="nav-items-container" id="nav-items-container">
                     <ul className="nav-items">
-                        <li> <Link to="/">Home</Link></li>
-                        <li> <Link to="/wakeparks">Wake Parks</Link></li>
+                        <li className={isActive ? 'active' : ''}> <Link onClick={toggleIsActive} to="/">Home</Link></li>
+                        <li className={isActive ? 'active' : ''}> <Link onClick={toggleIsActive} to="/wakeparks">Wake Parks</Link></li>
 
                         {username
                             ? (
                                 <>
                                     {/* <!-- loggedIn user --> */}
-                                    <li> <Link className="create" to="/wakeparks/create">Add a wakepark</Link></li>
+                                    <li className={isActive ? 'active' : ''}> <Link onClick={toggleIsActive} className="create" to="/wakeparks/create">Add a wakepark</Link></li>
                                     {/* <li> <Link className="profile" to="/profile">{username}'s Profile</Link></li> */}
-                                    <li> <Link className="logout" to="/logout">Log out</Link></li>
+                                    <li className={isActive ? 'active' : ''}> <Link onClick={toggleIsActive} className="logout" to="/logout">Log out</Link></li>
                                 </>
                             )
                             : (
                                 <>
                                     {/* <!-- all users --> */}
-                                    <li> <Link className="create" to="/wakeparks/create">Add a wakepark</Link></li>
-                                    <li> <Link className="login" to="/login">Log in</Link></li>
+                                    <li className={isActive ? 'active' : ''}> <Link onClick={toggleIsActive} className="create" to="/wakeparks/create">Add a wakepark</Link></li>
+                                    <li className={isActive ? 'active' : ''}> <Link onClick={toggleIsActive} className="login" to="/login">Log in</Link></li>
                                 </>
                             )
                         }
                     </ul>
+                    <div id="hamburger-menu" onClick={toggleIsActive}>
+                        <div className="bar" />
+                        <div className="bar" />
+                        <div className="bar" />
+                    </div>
                 </div>
             </nav>
         </header>
